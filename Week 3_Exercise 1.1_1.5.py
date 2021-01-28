5c81da740ce1f434fdc703bece3b57971ac6a705
dairantzis <dairantzis@yahoo.gr>
2021-01-28 16:48:50 +0000

    Add files via upload

---
+++ Week 3_Exercise 1.1_1.5.py
@@ -0,0 +1,28 @@
+
+
+
+print ('Welcome to my program! ')
+print ()
+print('Multiplication and division demo')
+print (3 * 4 * 5)                  # multiplication demo
+print () 
+print (44 // 3)                    # floor division demo
+print () 
+print (44 / 3)                     # division demo
+print () 
+print (44 % 3)                     # modulo demo
+print () 
+print('403 eggs make ',403//12,' full dozens and there are ',403%12,' eggs left', sep = '')
+print()
+print('Variable assignment demo')
+age = 20 
+job_title = 'cashier' 
+print (age) 
+print (job_title)
+print()
+print('Unicode demo')
+print ('\u03bb' + ' Greek small letter lambda')
+print ('\u0416' + ' Cyrillic capital letter Zhe')
+print ('\u00A3' + ' GBP sign')
+print()
+
